e587b91fbeb819d14a09e877e59558c301857a3f
Khoi Tuan Ngo <tuankhoi1977@gmail.com>
2024-08-02 11:12:23 +0700

    Add files via upload

---
+++ AddPanel/AddPanel/CsAddPanel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Media;
+
+namespace Walkthrough
+{
+    public class CsAddPanel : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("NewRibbonPanel");
+
+            // Create a push button to trigger a command and add it to the ribbon panel.
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            PushButtonData buttonData = new PushButtonData("cmdHelloWorld",
+                "Hello World", thisAssemblyPath, "Walkthrough.HelloWorld");
+
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+            // Optionally, other properties may be assigned to the button
+            // a) tool-tip
+            pushButton.ToolTip = "Say hello to the entire world.";
+
+            // b) large bitmap
+            string imagePath = Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "niga-logo-icon.png");
+            BitmapImage largeImage = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
+            pushButton.LargeImage = largeImage;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // nothing to clean up in this simple case
+            return Result.Succeeded;
+        }
+    }
+
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class HelloWorld : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
+            ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello World");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
